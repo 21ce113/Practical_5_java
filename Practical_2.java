018e268d587ac63b1ffb1541b3be2cebec1dc79c
21ce113 <97016155+21ce113@users.noreply.github.com>
2022-10-09 07:52:38 -0700

    Create Practical_2.java

---
+++ Practical_2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class Practical_2
+{
+public static void main(String[] args) throws IOException
+{
+FileReader sourceStream = null;
+try
+{
+sourceStream = new FileReader("D:\\java part 5\\Practical_2.txt");
+// reading from the file character by character
+int temp;
+while ((temp = sourceStream.read()) != -1)
+{
+System.out.println((char)temp);
+}
+}
+finally
+{ 
+// closing stream 
+if (sourceStream != null) 
+{
+sourceStream.close();
+} 
+}
+}
+}      
