0545e1873bd68a52e49baa9160444ecc4570c1d3
21ce113 <97016155+21ce113@users.noreply.github.com>
2022-10-09 07:54:47 -0700

    Create Practical_5.java

---
+++ Practical_5.java
@@ -0,0 +1,42 @@
+//ID : 21CE113
+
+/* * Aim : Write a program to enter any 15 numbers from the user and store only even numbers in a file named “Even.txt”. And display the contents of this file on the console. 
+(BufferedReader / BufferedWriter).*/
+import java.io.*;
+import java.util.*;
+public class Practical_5
+{
+public static void main(String[] args) throws IOException
+{
+// creating objects
+FileWriter writer = new FileWriter("D:\\java part 5\\Even.txt");
+BufferedWriter buffer = new BufferedWriter(writer);
+// getting 15 numbers from the user
+Scanner in = new Scanner(System.in);
+System.out.print("Enter 15 whole numbers: ");
+int[] num = new int[15];
+for(int i=0;i<15;i++)
+{
+num[i] = in.nextInt();
+}
+// printing even numbers in the file
+for(int i=0;i<15;i++) 
+{
+if(num[i]%2 == 0) 
+{
+buffer.write(num[i]);
+}
+}
+buffer.close();
+System.out.println("Content in the file is: ");
+// creating objects
+FileReader reader = new FileReader("D:\\java part 5\\Even.txt");
+BufferedReader read = new BufferedReader(reader);
+int i;
+// printing the content in the output
+while((i=read.read()) != -1)
+{
+System.out.println(i);
+}
+}
+}
